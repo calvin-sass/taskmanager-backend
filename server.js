6fcde0e443c526815700c02a86148c43cdaa6d39
Calvin Sass <calvinsass@outlook.com>
2025-06-03 01:09:52 +0200

    Fixed client path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware to hanle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -36,7 +36,7 @@ app.use("/api/reports", reportRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // For local development
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 }
